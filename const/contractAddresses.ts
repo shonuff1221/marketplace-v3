475eb72b13ff42dfac584bdbd6d30a10745b2755
Stephon <shonuff4521@gmail.com>
2023-07-07 19:50:56 -0400

    1st

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,17 +2,17 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+import { Goerli } from "@thirdweb-dev/chains";
+export const NETWORK = Goerli;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x6604bd9D7770035f26B4ACeab2C746fdCE166473";
+export const MARKETPLACE_ADDRESS = "0xCCf4FfB0429b56028E545cd64c9b201d6757ddfe";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0xFfd9bAddF3f6e427EfAa1A4AEC99131078C1d683";
+  "0x7620Ec69fAe4dB8bc23989810453707E1Db157a9";
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+export const ETHERSCAN_URL = "https://goerli.etherscan.io/";
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "",
+  
 };
 
 module.exports = nextConfig;
