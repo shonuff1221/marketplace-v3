79f35b0e8c5afc36f1a228b8b3f170a10ef780bf
Stephon <shonuff4521@gmail.com>
2023-07-23 16:51:56 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,16 +24,26 @@ const Home: NextPage = () => {
               />
             </div>
           </div>
+          <br></br>
+          <br></br>
+          <br></br>
           <div className={styles.heroAssetFrame}>
-            <Image
+          <iframe
+     src="https://bafybeidpp4d3znpjxlvlhitaylbqwmyk2gzcxazp5wkftvwu7r3lsyku2q.gateway.ipfscdn.io/?contract=0x3DCd997F81D032eC31b4d1f6f49228C9E645C5BA&chain=%7B%22name%22%3A%22BNB+Smart+Chain+Testnet%22%2C%22chain%22%3A%22BSC%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fbinance-testnet.rpc.thirdweb.com%2Fa8a7f8aa7ddd8115bad1892b179be99f%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22BNB+Chain+Native+Token%22%2C%22symbol%22%3A%22tBNB%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22bnbt%22%2C%22chainId%22%3A97%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22binance-testnet%22%7D&theme=dark&primaryColor=cyan"
+     width="600px"
+     height="600px"
+    
+    ></iframe>
+            {/* <Image
               src="/DD-NFT.gif"
-              width={512}
-              height={512}
+              width={860}
+              height={540}
               alt="Hero asset, NFT marketplace"
               quality={100}
               className={styles.heroAsset}
-            />
+            /> */}
           </div>
+          
           <div className={styles.heroBodyContainer}>
             <div className={styles.heroBody}>
               <h1 className={styles.heroTitle}>
