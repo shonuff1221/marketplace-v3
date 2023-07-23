dc97f72bbb97e65c72fb483c4de43d6331a75897
Stephon <shonuff4521@gmail.com>
2023-07-23 16:58:32 -0400

    Links Added

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -24,11 +24,20 @@ export function Navbar() {
           </Link>
 
           <div className={styles.navMiddle}>
+          <Link href="https://digital-dividends.net" className={styles.link}>
+              Trading
+            </Link>
             <Link href="/buy" className={styles.link}>
               Buy
             </Link>
             <Link href="/sell" className={styles.link}>
               Sell
+            </Link>
+            <Link href="https://beta.digital-dividends.net" className={styles.link}>
+              Beta
+            </Link>
+            <Link href="https://docs.digital-dividends.net" className={styles.link}>
+              Docs
             </Link>
           </div>
         </div>
