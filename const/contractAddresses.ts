d9f162b113aad8aa0a7f99ecd3e48698677bd89e
Stephon <shonuff4521@gmail.com>
2023-07-29 19:34:34 -0400

    Update contractAddresses.ts
    MainnetLaunch

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,13 +7,12 @@ export const NETWORK = Binance;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0xC62eFcdA8A4Cf074Ae873B789D35D9B8Fbe6d2d1";
+export const MARKETPLACE_ADDRESS = "0x8cA61C77154b1c37f1502A241D5c34DB1DE0903f";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0x9C6Cb0DA576E0CB1743Dbb86e951bd44a75aaD6c";
+  "0xF61B46Aa4b81803D41FCF9d14ee2fA4a1609001A";
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
 export const ETHERSCAN_URL = "https://bscscan.com/";
-
